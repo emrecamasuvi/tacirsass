870cf7fcccb330b48073be5441c34dc1925db0b1
Emre Çamaşuvi <emre@camasuvi.net>
2012-03-19 19:53:51 +0200

    refined sass, updated to 2.0.2

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
+http_path = "../../templates/admin"
+css_dir = "../admin/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "../admin/img"
+javascripts_dir = "../admin/js"
 output_style = :compact
 
 # You can select your preferred output style here (can be overridden via the command line):
