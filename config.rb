9ff13e7ab659cb02b63533a4b9955ea5139498fb
Emre Camasuvi <emre@camasuvi.net>
2012-04-11 12:32:56 +0300

    update

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "../../templates/admin"
-css_dir = "../admin/css"
+http_path = ""
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "../admin/img"
-javascripts_dir = "../admin/js"
+images_dir = "img"
+javascripts_dir = "js"
 output_style = :compact
 
 # You can select your preferred output style here (can be overridden via the command line):
